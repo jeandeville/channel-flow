43c4301ba9bee886fb1d171e75b042d1448f0052
saidouhamou <saidouhamou1@gmail.com>
2016-03-01 14:16:11 +0100

    algorithme k-omega premier jet

--- laplacien.py
+++ laplacien.py
@@ -53,28 +53,28 @@ def laplacien (y):
     
     
 
-def laplacien_turbulent (y, nu_t,nu,sigma): #Le laplacien turbulent n'est pas vraiment un laplacien, il s'agit du terme en dy((nu + sigma*nu_t) du/dy)
-    
-    Nb_Pts = np.size(y);
-    Nmax = Nb_Pts -1;
-    
-    dy = np.zeros(Nmax);
-    
-    for i in range(Nmax):
-        dy[i] = y[i+1] - y[i];
-        
-    A = np.zeros([Nb_Pts,Nb_Pts]);
-    
-    for i in range (1,Nb_Pts-1): #i va de 1 à Nb_Pts - 2
-        dy_moyen = 0.5 * (dy[i-1] + dy[i]);
-        nu_tsup = 0,5 * (nu_t[i+1] + nu_t[i]) #correspond à "Ci[i+0,5]"
-        nu_tinf = 0,5 * (nu_t[i] + nu_t[i-1]) #correspond à "Ci[i-0,5]"
-        
-        A[i,i] = -(nu+sigma*nu_tsup)/(dy[i+1]*dy_moyen)-(nu+sigma*nu_tinf)/(dy[i-1]*dy_moyen);
-        A[i,i-1] = (nu+sigma*nu_tinf)/(dy_moyen*dy[i-1]);
-        A[i,i+1] = (nu+sigma*nu_tsup)/(dy_moyen*dy[i]);
-        
-    return A;
+#def laplacien_turbulent (y, nu_t,nu,sigma): #Le laplacien turbulent n'est pas vraiment un laplacien, il s'agit du terme en dy((nu + sigma*nu_t) du/dy)
+#    
+#    Nb_Pts = np.size(y);
+#    Nmax = Nb_Pts -1;
+#    
+#    dy = np.zeros(Nmax);
+#    
+#    for i in range(Nmax):
+#        dy[i] = y[i+1] - y[i];
+#        
+#    A = np.zeros([Nb_Pts,Nb_Pts]);
+#    
+#    for i in range (1,Nb_Pts-1): #i va de 1 à Nb_Pts - 2
+#        dy_moyen = 0.5 * (dy[i-1] + dy[i]);
+#        nu_tsup = 0,5 * (nu_t[i+1] + nu_t[i]) #correspond à "Ci[i+0,5]"
+#        nu_tinf = 0,5 * (nu_t[i] + nu_t[i-1]) #correspond à "Ci[i-0,5]"
+#        
+#        A[i,i] = -(nu+sigma*nu_tsup)/(dy[i+1]*dy_moyen)-(nu+sigma*nu_tinf)/(dy[i-1]*dy_moyen);
+#        A[i,i-1] = (nu+sigma*nu_tinf)/(dy_moyen*dy[i-1]);
+#        A[i,i+1] = (nu+sigma*nu_tsup)/(dy_moyen*dy[i]);
+#        
+#    return A;
     
     
 def inverse_tdgauche_turbulent (dt, beta, omega): #correspond à l'inverse du terme de gauche (1+dt*Beta*omega(i)); c'est une matrice diagonale
@@ -99,7 +99,7 @@ def N_turbulent(U,nu_t,y): #correspond au terme de droite "N" du rapport, corres
         dy[i] = y[i+1] - y[i];
         
         
-    if np.size(nu_t):
+    if np.size(nu_t)==1:
         nu = nu_t * np.ones(Nb_Pts); #coorespond à l'équation de omega: on a une constante gamme au lieu d'un vecteur de nu_t
     else:
         nu = nu_t; #correspond à l'équation de k
--- main_turbulent.py
+++ main_turbulent.py
@@ -16,16 +16,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from maillage import *;
 from laplacien import *;
+from Initialisation import *
 
 #   Paramètres
-h = 1 # taille de la moitie du canal
+
+#   Paramètres
 r = 1.01 # taux de croissance du maillage
+dy0= 0.01;# pas de la première cellule 
+h = 1 # taille de la moitie du canal
+gradient=-1.0; # gradient de pression modifié (1/rho * dPe/dx)
+Rstar=100.0; # nombre de rynolds (Rstar=u_tau*h/nu)
+u_tau=np.sqrt(h*(-gradient));# vitesse caractéristique de frottement (u_tau=mu.dU/dy|y=0) 
+nu=u_tau*h/Rstar; # viscosité dynamique à 0 C
 Niter = 300; # nombre d'itérations
 dt = 10.; # pas de calcul
-dy0= 0.01;# pas de la première cellule 
-gradient=-1.0; # gradient de pression modifié (1/rho * dPe/dx)
-nu=1.75*10**-3; # viscosité dynamique à 0 C
-
+####################
+####### IL FAUT RAJOUTER LES CONSTANTES SIGMA_K SIGMA_OMEGA ET GAMMA
+#######################
 
 #création du maillage
 y = maillage(h,r,dy0);
@@ -33,6 +40,9 @@ y = maillage(h,r,dy0);
 # Def Nb_Pts
 Nb_Pts = np.size(y);
 Nmax = Nb_Pts-1;
+
+#Def de Gr
+Gr = gradient * np.ones(Nb_Pts)
 
 #Solution laminaire
 Ulaminaire = 1/(2*nu)*gradient*np.multiply(y,np.add(y,-2*h));
@@ -42,24 +52,32 @@ Ulaminaire = 1/(2*nu)*gradient*np.multiply(y,np.add(y,-2*h));
 #Initialisation (de k0, omega0, u0)
 ######
 
+Init=initialisation(y,h,Rstar,gradient);
+U0=Init[0,:];
+nu_t0=Init[1,:];
+k0=Init[2,:];
+omega0=Init[3,:];
 
+#plt.plot(y,u0)
+#print(u0)
+#print(k0)
+#print(nu_t0)
+#print(omega0)
 
+Uprec = U0;
+kprec = k0;
+omegaprec = omega0;
+nu_tprec = nu_t0;
+Id = np.eye(Nb_Pts);
 
-
-
-
-
-
+################## DEBUT BOUCLE ##############
 
 
 ######### 
 #Mise à jour du vecteur U_n+1:
 #U_n+1 = -dt* Gr + (Id +dt * L(nu_t)).U_n
 ########
-
-
-
-
+U = -dt * Gr + np.multiply((Id + dt * L_turbulent(nu,nu_t,1,y)),Uprec); #on envoie 1 car formule de L_Turbulent: nu + sigma*nu_t
 
 
 
@@ -67,9 +85,9 @@ Ulaminaire = 1/(2*nu)*gradient*np.multiply(y,np.add(y,-2*h));
 #Mise à jour vecteur K
 # K_n+1 = (Id + dt * M(Beta_etoile,omega))^-1 * (dt*N(U_n,nu_t_n) + (Id + dt * L(nu, nu_t_n,sigma_k)).K_n )
 ##########
-
-
-
+inverse_matrix = inverse_tdgauche_turbulent (dt, beta_star, omegaprec);
+second_terme = dt *  N_turbulent(Uprec,nu_tprec,y) + np.multiply(Id + dt * L_turbulent(nu,nu_tprec,sigma_k,y),kprec);
+K = np.multiply(inverse_matrix,second_terme);
 
 
 
@@ -77,12 +95,19 @@ Ulaminaire = 1/(2*nu)*gradient*np.multiply(y,np.add(y,-2*h));
 #Mise à jour vecteur W
 # W_n+1 = (Id + dt * M(Beta,omega))^-1 * (dt*N(U_n,gamma) + (Id + dt * L(nu, nu_t_n,sigma_omega)).W )
 ##########
+inverse_matrix = inverse_tdgauche_turbulent (dt, beta, omegaprec);
+second_terme = dt *  N_turbulent(Uprec,gamma,y) + np.multiply(Id + dt * L_turbulent(nu,nu_tprec,sigma_omega,y),omegaprec);
+omega = np.multiply(inverse_matrix,second_terme);
 
 
+##########
+#Mise à jour nu_t
+# nu_t = k / omega
+#########
+nu_t = np.multiply(K,np.power(omega,-1));
 
 
-
-
+################## FIN BOUCLE ##############
 
 
 
