33bf1411686e2d71adbbb19bce0a23cfab61266e
jeandeville <deville.jean@gmail.com>
2016-03-14 20:15:18 +0100

    mise à jour

--- main_turbulent_debug.py
+++ main_turbulent_debug.py
@@ -22,7 +22,7 @@ from thomas import *;
 
 #   Paramètres
 r = 1.01 # taux de croissance du maillage
-Niter = 1; # nombre d'itérations
+Niter = 1000; # nombre d'itérations
 dt = 10.; # pas de calcul
 
 #Entrées versions 1
@@ -104,6 +104,7 @@ u0plus=1.0/u_tau*U0
 K0plus = K0/(0.5*u_tau**2)
 nu_t0plus=nu_t0/nu
 yplus=y*Rstar/h
+
 #Lois de paroi
 kappa = 0.41;
 Cplus = 5.0;
@@ -119,8 +120,14 @@ nu_tprec = nu_t0;
 Id_tid = np.eye(Nb_Pts); #correspond à une matrice identité prenant en compte lesCAL c'est à dire dernièreligne à 0...0 -1 1 ("I_tild")
 Id_tid[Nmax,Nmax-1] =-1;
 
+#résidus
+Courbe_RsdU = np.ones(Niter); #vitesse
+Courbe_Rsdnu_t = np.ones(Niter);
+Courbe_RsdK = np.ones(Niter);
+axis = np.ones(Niter);
 
-
+for i in range(Niter):
+    axis[i] = i;
 
 
 
@@ -135,7 +142,6 @@ for i in range(Niter):
     b_u=Uprec-dt * Gr;
     b_u[0]=0.0;
     b_u[Nmax]=0.0;
-    #U = np.dot(np.linalg.inv(A_u), b_u) #on envoie 1 car formule de L_Turbulent: nu + sigma*nu_t
     U = thomas(A_u, b_u)
     
     ##########
@@ -163,6 +169,23 @@ for i in range(Niter):
     # nu_t = k / omega
     #########
     nu_t = np.multiply(K,np.power(W,-1));
+    
+
+    ##########
+    # Calcul de résidus
+    ##########
+    Rsd_u = U - Uprec;
+    Rsd_k = K - Kprec;    
+    Rsd_nu_t = nu_t - nu_tprec;
+    
+    Courbe_RsdU[i] = np.linalg.norm(Rsd_u);
+    Courbe_Rsdnu_t[i] = np.linalg.norm(Rsd_nu_t)
+    Courbe_RsdK[i]= np.linalg.norm(Rsd_k)
+    
+    #Rsd_global[i] = np.max(Rsd);
+    #Rsd_global([i]) = log 10 np.max(Rsd);
+    
+
 
     ###########
     #Transformation des résultats sont grandeurs adimensionnées
@@ -171,85 +194,109 @@ for i in range(Niter):
     kplus=K/(0.5*u_tau**2)
     nu_tplus = nu_t/nu    
     
-    #plt.plot(yplus,nu_t);
-    #plt.plot(yplus,k);
-    #plt.plot(W);
-    #plt.show();
-    #plt.pause(0.0001)
+
     
     
     ######### PARTIE PLOT #############
     
-    ## comparaison DNS /k-w pour u+ ###
-    plt.clf()
-    #plt.plot(yplusDNS,uplusDNS,'g',label='DNS');
-    #plt.plot(yplus,uplus,'r',label='k-w'); 
-    #plt.plot(yplus,u0plus,'b',label='mixing length');
-    #plt.plot(yplus[1:80],Uwall_sublayer[1:80],'y',label='wall law');
-    #plt.plot(yplus[60:Nmax],Uwall_loglayer[60:Nmax],'y');
-    plt.plot(yplus[1:160],Uwall_sublayer[1:160],'y',label='wall law');
-    plt.plot(yplus[120:Nmax],Uwall_loglayer[120:Nmax],'y');    
-    plt.xscale('log')
-    plt.ylim((0,20))
-    plt.xlim((0,190))
-    plt.title('U+=f(y+) DNS:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
-    #plt.title('U+=f(y+) DNS:    Re_tau = 180, 1/nu = 3250; u_tau = 0.057231059')
-    plt.legend(loc=2);
-    plt.show()
-    plt.pause(0.0001)
-    
-    
-###    ### comparaison SST / k-w pour u+ ###
+#    ## comparaison DNS /k-w pour u+ ###
 #    plt.clf()
-#    plt.plot(yplusSST,uplusSST,'g',label='SST');
+#    f1 = plt.subplot(1,2,1)
+#    plt.plot(yplusDNS,uplusDNS,'g',label='DNS');
 #    plt.plot(yplus,uplus,'r',label='k-w'); 
 #    plt.plot(yplus,u0plus,'b',label='mixing length');
 #    #plt.plot(yplus[1:80],Uwall_sublayer[1:80],'y',label='wall law');
 #    #plt.plot(yplus[60:Nmax],Uwall_loglayer[60:Nmax],'y');
-#    plt.plot(yplus[1:160],Uwall_sublayer[1:160],'y',label='wall law');
-#    plt.plot(yplus[140:Nmax],Uwall_loglayer[140:Nmax],'y');
+#    plt.plot(yplus[1:160],Uwall_sublayer[1:160],'y');
+#    plt.plot(yplus[120:Nmax],Uwall_loglayer[120:Nmax],'y');    
 #    plt.xscale('log')
 #    plt.ylim((0,20))
 #    plt.xlim((0,190))
-#    plt.title('U+=f(y+) SST:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
-#    
+#    plt.title('U+=f(y+) DNS:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
+#    #plt.title('U+=f(y+) DNS:    Re_tau = 180, 1/nu = 3250; u_tau = 0.057231059')
 #    plt.legend(loc=2);
+#    #plt.show()
+#    #plt.pause(0.0001)
+#    #plt.clf()
+    
+
+    
+
+
+
+
+    
+    
+##    ### comparaison SST / k-w pour u+ ###
+    plt.clf()
+    f1 = plt.subplot(2,2,1)
+    plt.plot(yplusSST,uplusSST,'g',label='SST');
+    plt.plot(yplus,uplus,'r',label='k-w'); 
+    plt.plot(yplus,u0plus,'b',label='mixing length');
+    #plt.plot(yplus[1:80],Uwall_sublayer[1:80],'y',label='wall law');
+    #plt.plot(yplus[60:Nmax],Uwall_loglayer[60:Nmax],'y');
+    plt.plot(yplus[1:160],Uwall_sublayer[1:160],'y',label='wall law');
+    plt.plot(yplus[140:Nmax],Uwall_loglayer[140:Nmax],'y');
+    plt.xscale('log')
+    plt.ylim((0,20))
+    plt.xlim((0,190))
+    plt.title('U+=f(y+) SST:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
+    
+    plt.legend(loc=2);
 #    plt.show()
 #    plt.pause(0.0001)
     
     
 
-#    ## comparaison SST / k-w pour k+ ###
-#    plt.clf()
-#    plt.plot(yplusSST,kplusSST,'g',label='SST');
-#    plt.plot(yplus,kplus,'r',label='k-w'); 
-#    plt.plot(yplus,K0plus,'b',label='mixing length');
-#    #plt.xscale('log')
-#    plt.ylim((-1,9))
-#    plt.xlim((0,1000))
-#    plt.title('K+=f(y+) SST:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
-#    
-#    plt.legend(loc=1);
+    ## comparaison SST / k-w pour k+ ###
+    #plt.clf()
+    f2 = plt.subplot(2,2,2)
+    plt.plot(yplusSST,kplusSST,'g',label='SST');
+    plt.plot(yplus,kplus,'r',label='k-w'); 
+    plt.plot(yplus,K0plus,'b',label='mixing length');
+    #plt.xscale('log')
+    plt.ylim((-1,9))
+    plt.xlim((0,1000))
+    plt.title('K+=f(y+) SST:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
+    
+    plt.legend(loc=1);
 #    plt.show()
 #    plt.pause(0.0001)
 #    
     
 
-#    ### comparaison SST / k-w pour nu_t + ###    
-#    plt.clf()
-#    plt.plot(yplusSST,nu_tplusSST,'g',label='SST');
-#    plt.plot(yplus,nu_tplus,'r',label='k-w'); 
-#    plt.plot(yplus,nu_t0plus,'b',label='mixing length');
-#    #plt.xscale('log')
-#    plt.ylim((-20,160))
-#    plt.xlim((0,1000))
-#    plt.title('nu_t=f(y+) SST:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
-#    
-#    plt.legend(loc=2);
+    ### comparaison SST / k-w pour nu_t + ###    
+    #plt.clf()
+    f3 = plt.subplot(2,2,3)
+    plt.plot(yplusSST,nu_tplusSST,'g',label='SST');
+    plt.plot(yplus,nu_tplus,'r',label='k-w'); 
+    plt.plot(yplus,nu_t0plus,'b',label='mixing length');
+    #plt.xscale('log')
+    plt.ylim((-20,160))
+    plt.xlim((0,1000))
+    plt.title('nu_t=f(y+) SST:    Re_tau = 950, 1/nu = 20580; u_tau = 0.045390026')
+    
+    plt.legend(loc=2);
 #    plt.show()
 #    plt.pause(0.0001)    
     
     
+    
+    
+    f4 = plt.subplot(2,2,4)
+    plt.plot(axis[0:i],Courbe_RsdU[0:i],'green',label='residuals U');
+    plt.plot(axis[0:i],Courbe_RsdK[0:i],'red',label='residuals k ');
+    plt.plot(axis[0:i],Courbe_Rsdnu_t[0:i],'blue',label='residuals nu_t');
+    plt.legend(loc=1)
+    plt.xlim((0,Niter))
+    plt.yscale('log')
+   
+
+    
+
+    
+    plt.show()
+    plt.pause(0.0001)
     
     
     
